558028dd4d55f39be509d224503237a48ff888ac
atm4god <tmarvellous41@gmail.com>
2024-12-24 22:01:20 +0100

    Add dynamic registration for all models in Django admin
    This commit introduces dynamic model registration in the admin site by leveraging a mixin to set `list_display` and `search_fields` automatically. Models are registered unless already registered or explicitly excluded, such as "FCMDevice". This simplifies admin setup and ensures all applicable models are visible.

---
+++ annotation/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from django.apps import apps
+from django.contrib import admin
+
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        self.search_fields = [field.name for field in model._meta.fields if not field.is_relation]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+
+models = apps.get_models()
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        if str(model.__name__) == "FCMDevice":
+            continue
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+# Register your models here.
